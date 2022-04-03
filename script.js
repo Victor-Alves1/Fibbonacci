5395ca570b876d34f144883bb9ea72e5619dd23e
Victor Alves da Costa <victoralves741@gmail.com>
2022-04-03 10:50:36 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(){
+    let fibbo = window.document.querySelector(`input#fibbo`)
+    let res = window.document.querySelector(`div.res`)
+    let res1 = window.document.querySelector(`div.res1`)
+    let res2 = window.document.querySelector(`div.res2`)
+    let nt = Number(fibbo.value)
+
+            let n0=0
+            let n1=0
+            let n2=1
+            if (nt>1){
+                    res.innerHTML = `<p>Sequência de número "n"   |</p>`
+                    res1.innerHTML = `<p>Números de fibbonacci</p>`
+                    res2.innerHTML = `<p>|   dois elevado a "n"</p>`
+                for (let c = 1; c<=nt; c+= 1){
+                    pot= 2**c
+                    res.innerHTML += `<p> ${c}º  </p>`
+                    res1.innerHTML += `<p>  ${n2}  </p>`
+                    res2.innerHTML += `<p>  ${pot} </p>`
+                    n0= n1
+                    n1= n2
+                    n2= n1+ n0
+                }
+            } else {
+                window.alert(`Número inválido`)
+            }
+            num.focus()
+}
